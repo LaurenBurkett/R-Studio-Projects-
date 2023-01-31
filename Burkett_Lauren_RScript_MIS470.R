aa163b82416feaa561e287134e618bc8522d6e58
Lauren Burkett <laurenelizabethburkett@gmail.com>
2023-01-31 14:53:22 -0400

    R Script Creation Of Table

---
+++ Burkett_Lauren_RScript_MIS470.R
@@ -0,0 +1,17 @@
+my_names <- c("lauren", "kyle", "ada")
+> my_names
+[1] "lauren" "kyle"   "ada"   
+> my_ID <- 1:3
+> my_ID  
+[1] 1 2 3
+> my_state <- c("SC", "MD", "VA")
+> my_state
+[1] "SC" "MD" "VA"
+> my_data <- data.frame(my_names, my_ID, my_state)
+> my_data
+my_names my_ID my_state
+1   lauren     1       SC
+2     kyle     2       MD
+3      ada     3       VA
+
+
